8e72b6e36b4bc529807877da2ca343a03ec7bb06
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2019-05-18 22:45:37 +0300

    Add empty UpperBar component

---
+++ src/components/UpperBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UpperBar.css';
+
+const UpperBar: React.FC = () => {
+  return (
+    <div className="UpperBar">
+
+    </div>
+  );
+}
+
+export default UpperBar;
