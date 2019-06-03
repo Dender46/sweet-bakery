991199e21c6f4d818abc0ae7addc19e6f81ede0f
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2019-06-03 22:26:51 +0300

    Refactor UpperBar to use Hooks

--- src/components/UpperBar.tsx
+++ src/components/UpperBar.tsx
@@ -4,32 +4,28 @@ import './Avatar.css';
 
 import 'material-design-icons/iconfont/material-icons.css';
 
-class UpperBar extends React.Component<{ toggleProfilePopup: Function }> {
-  render() {
-    return (
-      <div className="UpperBar">
-        <h1 id="brand-name">Sweet Bakery</h1>
+function UpperBar(props : {toggleProfilePopup : Function}) {
 
-        <div className="bg-drips">
-          <div id="bg-drip-left"></div>
-          <div id="bg-drip-right"></div>
-        </div>
-        <Avatar toggleProfilePopup={this.props.toggleProfilePopup} />
+  return (
+    <div className="UpperBar">
+      <h1 id="brand-name">Sweet Bakery</h1>
+
+      <div className="bg-drips">
+        <div id="bg-drip-left"></div>
+        <div id="bg-drip-right"></div>
       </div>
-    );
-  }
-
+      <Avatar toggleProfilePopup={props.toggleProfilePopup} />
+    </div>
+  );
 }
 
-class Avatar extends React.Component<{ toggleProfilePopup: Function }> {
-  render() {
-    return (
-      <div className="Avatar" onClick={() => this.props.toggleProfilePopup()}>
-        <img src='images/avatar-icon.jpg' alt="Profile" className="avatar-icon" />
-        <i className="down-arrow material-icons expand_more">expand_more</i>
-      </div>
-    );
-  }
+function Avatar (props : { toggleProfilePopup: Function }) {
+  return (
+    <div className="Avatar" onClick={() => props.toggleProfilePopup()}>
+      <img src='images/avatar-icon.jpg' alt="Profile" className="avatar-icon" />
+      <i className="down-arrow material-icons expand_more">expand_more</i>
+    </div>
+  );
 }
 
 export default UpperBar;
