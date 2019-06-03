be23e66f1bb216937d2e312b8b6c6f6efd11070a
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2019-06-03 22:24:04 +0300

    Refactor ProfilePopup to use Hooks

--- src/components/ProfilePopup.tsx
+++ src/components/ProfilePopup.tsx
@@ -1,82 +1,23 @@
-import React from "react";
-import "./ProfilePopup.css";
+import React, {useState, useEffect} from 'react';
+import './ProfilePopup.css';
 
-type ProfilePopupProps = {
-  mounted: boolean;
-};
+function ProfilePopup(props : {mounted : boolean}) : any {
+    const [opacity, setOpacity] = useState(0);
+    const [show, setShow] = useState(false);
 
-type ProfilePopupState = {
-  show: boolean;
-  style: {
-    opacity: number;
-  };
-};
+    useEffect( () => {
+        if (props.mounted)
+            setShow(true);
+        setTimeout( () => setOpacity(props.mounted ? 1 : 0), 10);
+    }, [props.mounted]);
 
-class ProfilePopup extends React.Component<
-  ProfilePopupProps,
-  ProfilePopupState
-> {
-  constructor(props: ProfilePopupProps) {
-    super(props);
-    this.state = {
-      show: false,
-      style: {
-        opacity: 0
-      }
-    };
-  }
-
-  mountStyle = () => {
-    if (!this.props.mounted) return;
-
-    this.setState({
-      style: {
-        opacity: 1
-      }
-    });
-  };
-
-  unMountStyle = () => {
-    this.setState({
-      style: {
-        opacity: 0
-      }
-    });
-  };
-
-  componentDidMount = () => {
-    setTimeout(this.mountStyle, 10);
-  };
-
-  componentWillReceiveProps(newProps: ProfilePopupProps) {
-    if (!newProps.mounted) return this.unMountStyle();
-
-    this.setState({
-      show: true
-    });
-    setTimeout(this.mountStyle, 10);
-  }
-
-  transitionEnd = () => {
-    if (!this.props.mounted) {
-      // remove the node on transition end when the mounted prop is false
-      this.setState({
-        show: false
-      });
-    }
-  };
-
-  render() {
     return (
-      this.state.show && (
-        <div
-          className="ProfilePopup"
-          style={this.state.style}
-          onTransitionEnd={this.transitionEnd}
+        show && <div
+            className="ProfilePopup"
+            style={{opacity: opacity}}
+            onTransitionEnd={ () => setShow(props.mounted)}
         />
-      )
     );
-  }
 }
 
 export default ProfilePopup;
