26f989ad404fc31554cbe7e69d2b97cea21233d5
Dmitriy Bagandov <dmitriy.bagandov@gmail.com>
2019-07-14 14:57:29 +0300

    Add profilePopup elements without css

--- src/components/ProfilePopup.tsx
+++ src/components/ProfilePopup.tsx
@@ -1,6 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import './ProfilePopup.css';
 
+const profilePopupElements : Array<{icon : String, caption : String}> = [
+    {icon: "shopping_cart",             caption: "Cart"},
+    {icon: "settings",                  caption: "Preferences"},
+    {icon: "subdirectory_arrow_right",  caption: "Sign Out"},
+    {icon: "help_outline",              caption: "Help"}
+]
+
 function ProfilePopup(props : {mounted : boolean, profileName : String}) : any {
     const [opacity, setOpacity] = useState(0);
     const [show, setShow] = useState(false);
@@ -22,8 +29,18 @@ function ProfilePopup(props : {mounted : boolean, profileName : String}) : any {
             onTransitionEnd={ () => setShow(props.mounted)}
         >
             <h1 className="profile-name">{props.profileName}</h1>
+            {profilePopupElements.map((props) => ( <ProfilePopupElement icon={props.icon} caption={props.caption} /> ))}
         </div>
     );
+}
+
+function ProfilePopupElement(props : {icon : String, caption : String}) {
+    return (
+        <div className="ProfilePopupElement">
+            <i className="material-icons noselect">{props.icon}</i>
+            <h3 className="section-name noselect">{props.caption}</h3>
+        </div>
+    )
 }
 
 export default ProfilePopup;
