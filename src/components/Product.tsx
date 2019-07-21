913ad5bd4aa0dd973d2b989029c10ad757eec5e8
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2019-07-22 00:53:53 +0300

    Refine css and add price tag for products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -12,7 +12,7 @@ function Product( props : {image: String, title:String} ) : any {
             <i className="info-button material-icons noselect" >add_circle_outline</i>
             
             <div style={{opacity: overlayOpacity}} className="prod-overlay">
-                
+                <h2 className="prod-price">450 грн.</h2>
             </div>
         </div>
     )
