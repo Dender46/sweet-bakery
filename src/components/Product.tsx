4b8445441ad87eb7454d9a8b941f4afdd9f535d3
Dmitry Bagandov <dmitriy.bagandov@gmail.com>
2019-07-22 00:40:17 +0300

    Stop being a retard and read more about capturing MouseEvents

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.css';
 
 function Product( props : {image: String, title:String} ) : any {
+    const [overlay, setOverlay] = useState(false);
+
     return (
-        <div className="Product">
+        <div className="Product" onMouseOver={() =>setOverlay(true)} onMouseLeave={() =>setOverlay(false)}>
             <img className="prod-image noselect" alt="" src={require("../images/cakes/" + props.image)}/>
             <br />
             <h2  className="prod-title" > {props.title} </h2>
             <i className="info-button material-icons noselect" >add_circle_outline</i>
             {
-                <div className="prod-overlay">
+                overlay && <div className="prod-overlay">
                     
                 </div>
             }
